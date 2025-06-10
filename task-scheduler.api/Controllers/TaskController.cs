bd3565371569233651279148c317f2b3c7adc58b
Prockentine <prockentine@gmail.com>
2025-06-09 21:14:00 -0400

    created api project for task scheduler

---
+++ task-scheduler.api/Controllers/TaskController.cs
@@ -0,0 +1,27 @@
+using Microsoft.AspNetCore.Mvc;
+
+namespace task_scheduler.Controllers;
+
+[ApiController]
+[Route("tasks")]
+public class TaskController(ILogger<TaskController> logger) : ControllerBase
+{
+    private static readonly string[] Summaries =
+    [
+        "Freezing", "Bracing", "Chilly", "Cool", "Mild", "Warm", "Balmy", "Hot", "Sweltering", "Scorching"
+    ];
+
+    private readonly ILogger<TaskController> _logger = logger;
+
+    [HttpGet(Name = "GetWeatherForecast")]
+    public IEnumerable<WeatherForecast> Get()
+    {
+        return Enumerable.Range(1, 5).Select(index => new WeatherForecast
+            {
+                Date = DateOnly.FromDateTime(DateTime.Now.AddDays(index)),
+                TemperatureC = Random.Shared.Next(-20, 55),
+                Summary = Summaries[Random.Shared.Next(Summaries.Length)]
+            })
+            .ToArray();
+    }
+}
